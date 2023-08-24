afa0cfc451b0585facb20d3fa8235775eb3856ec
Deven Gangwani <devengangwani12@gmail.com>
2023-08-24 21:37:21 +0530

    Added dictionary route, GET for root

--- server.mjs
+++ server.mjs
@@ -1,3 +1,27 @@
 import express from "express";
 import * as dotenv from "dotenv";
 
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+const games = ["wordle", "hangman"];
+
+let dictionary = process.env.DICTIONARY.split("_");
+
+app.post("/dictionary", (req, res) => {
+	//accept a json array of words, set dictionary to it
+	if (!Array.isArray(req.body)) {
+		res.status(400).json("Not an array");
+		return;
+	}
+	dictionary = req.body;
+	res.status(200).json("OK");
+});
+
+app.get("/", (req, res) => {
+	res.json(games);
+});
+
+app.listen(3000);
