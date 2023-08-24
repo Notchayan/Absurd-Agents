b544c6ca17f434c615f3751a9f3533cbb95ed806
Sameer Yadav <sameer.yadv8@gmail.com>
2023-08-24 21:56:18 +0530

    implemented express-session

--- server.mjs
+++ server.mjs
@@ -1,3 +1,29 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import session from "express-session";
 
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(session({
+    secret: 'helloWorld',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge:30*24*60*60 }
+  }))
+
+
+app.get('/',(req,res)=>{
+    console.log(req.session.views);
+    if (req.session.views) {
+        res.send('You are logged in')
+    } else {
+        res.send('You are not logged')
+        req.session.views=1
+        console.log(req.session.views);
+        req.session.save()
+    }
+})
+
+
+app.listen(port,()=>{console.log(`Listening on Port ${port}`);})
